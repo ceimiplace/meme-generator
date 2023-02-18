32a6b006500476e4726193c8271e3249362d0efa
ceimiplace <azamfiredenis@yahoo.com>
2023-02-18 12:36:42 +0200

    added data object

--- src/Meme.js
+++ src/Meme.js
@@ -1,11 +1,15 @@
 import "./Meme.css";
+import memeData from "./memeData";
 export default function Meme() {
+  function handleClick() {
+    console.log(memeData.data.memes[Math.floor(Math.random() * 101)].url);
+  }
   return (
     <main className="main--container">
-      <form className="main--form">
+      <form className="main--form" onSubmit={(e) => e.preventDefault()}>
         <input placeholder="Top text" className="form-input"></input>
         <input placeholder="Bottom text" className="form-input"></input>
-        <button className="main-form--submit-button">
+        <button className="main-form--submit-button" onClick={handleClick}>
           Get a new meme image
         </button>
       </form>
